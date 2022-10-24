af0a067f43b78c3af3d651dcf4703ea2e9d39402
Gabor Jenei <jeneigabor14@gmail.com>
2022-10-24 08:32:11 +0100

    Update docstrings

--- tests/test_macaroni.py
+++ tests/test_macaroni.py
@@ -1,12 +1,16 @@
-import sys
-sys.path.append('../')
 import pandas as pd
 import numpy as np
 from pandas import testing as tm
+
+import sys
+sys.path.append('../')
 from macaroni.data import make_data
 
 
 def test_decompose_name_1():
+    """
+    Test a name with 3 components: title, first name and last name
+    """
     name = 'Mr. Brandon Thornton'
     result = pd.Series(['Mr.', 'Brandon', 'Thornton'])
     output = make_data.decompose_name(name)
@@ -15,6 +19,9 @@ def test_decompose_name_1():
 
 
 def test_decompose_name_2():
+    """
+    Test a name with 2 components: first name and last name
+    """
     name = 'Joel Allen'
     result = pd.Series([np.NaN, 'Joel', 'Allen'])
     output = make_data.decompose_name(name)
@@ -23,6 +30,9 @@ def test_decompose_name_2():
 
 
 def test_decompose_name_3():
+    """
+    Test a name with 4 components: first name, middle name and last name
+    """
     name = 'Ms. Catherine Zeta Jones'
     result = pd.Series(['Ms.', 'Catherine', 'Jones'])
     output = make_data.decompose_name(name)
